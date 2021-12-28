0974fd45b12d2ad68f0d0b6b27fdd8e13f8c5d01
Tim Sakhaulov <timur.sakhaulov@gmail.com>
2021-12-28 08:03:26 +0500

    Added GUI

--- src/gui/windowParts/panelParts/EntityInfoPart.java
+++ src/gui/windowParts/panelParts/EntityInfoPart.java
@@ -18,25 +18,22 @@ public class EntityInfoPart extends PanelPart {
     protected int level_value = 100;
     protected int hp_value = 100;
     protected int attack_value = 100;
-    protected int defence_value = 100;
 
     public EntityInfoPart() {
 
-        setLayout(new GridLayout(6, 1));
+        setLayout(new GridLayout(5, 1));
 
         title_label = new JLabel("=ENTITY INFO=", SwingConstants.CENTER);
         name_label = new JLabel("Name:    "+name_value);
         level_label = new JLabel("Level:  "+level_value);
         hp_label = new JLabel("HP:    "+hp_value);
         attack_label = new JLabel("Attack:    "+attack_value);
-        defence_label = new JLabel("Defence:  "+defence_value);
 
         add(title_label);
         add(name_label);
         add(level_label);
         add(hp_label);
         add(attack_label);
-        add(defence_label);
 
     }
 
